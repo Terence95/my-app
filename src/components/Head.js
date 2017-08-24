3e9682f79b72f2364d17133cb34436c4ff46ce73
Terence <wrze@sina.com>
2017-08-24 20:23:21 +0800

    add Head.js

---
+++ src/components/Head.js
@@ -0,0 +1,15 @@
+import React from 'React'
+
+const Head = ({isFullScreen}) => {
+    return (
+        <header className="header-wrap">
+            <h1 className="logo">Ter note</h1>
+            <ul className="head-nav">
+                <li><a href="">blog</a></li>
+                <li><a href="">Git</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Head
