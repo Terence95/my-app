e08c8365fa10f2bae2dc3e846d18a8cdc2357516
Terence <wrze@sina.com>
2017-08-24 21:46:28 +0800

    finish header

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Head = ({isFullScreen}) => {
     return (
         <header className="header-wrap">
-            <h1 className="logo">Ter note</h1>
+            <h1 className="logo">TerNote</h1>
             <ul className="head-nav">
-                <li><a href="">blog</a></li>
+                <li><a href="">Blog</a></li>
                 <li><a href="">Git</a></li>
             </ul>
         </header>
