7094611e7bf8e3fa572024f503f0cfd2aaf21045
Terence <wrze@sina.com>
2017-08-24 19:56:10 +0800

    add note.js

---
+++ src/actions/note.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store/store';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import 'element-theme-default';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
+registerServiceWorker();
