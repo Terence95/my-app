70e42f546d0f30506ddcb7cd99594e1667a6c210
Terence <wrze@sina.com>
2017-08-25 14:37:16 +0800

    actions note.js add note and add 'addNote' action creator

--- src/actions/note.js
+++ src/actions/note.js
@@ -1,5 +1,7 @@
 import moment from 'moment'
 import { getStorage } from '../utils/storage'
+// 这些相当于action的type,也就是action的名称, action描述当前发生的事情,改变state的唯一方法,使用action,将数据运送到store
+// Action Creator, view要发送多少种消息,就会有多少种Action。如果手写会很麻烦,可以定义一个函数来生成Action 这个函数叫Action Creator
 import { ADD_NOTE, DELETE_NOTE, SHOW_NOTE, SHOW_LAYER, SHOW_EDITER } from '../constants'
 
 // Action State的变化,会导致View的变化。但是,用户接触不到State,只能接触到View, 因此,State的变化必须是View导致的,
@@ -20,6 +22,28 @@ if (notesArr instanceof Array && notesArr.length !== 0) {
     noteId = notesArr2[0]['id']+1;
 } else {
     noteId = 0;
+}
+
+// 增加一篇笔记 或者 编辑之后保存
+// 这个addNote是一个Action Creator
+export const addNote = function (title, content, id, time) => {
+    if (id === undefined && time === undefined) {
+        return {
+            type: ADD_NOTE,
+            id: noteId++,
+            title,
+            content,
+            time:moment().format("YYYY-MM-DD HH:mm")
+      }
+    } else {
+        return {
+            type: ADD_NOTE,
+            id,
+            title,
+            content,
+            time: moment().format("YYYY-MM-DD HH:mm")
+        }
+    }
 }
 
 
